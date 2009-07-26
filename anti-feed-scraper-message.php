a97fa29f9731aef44407ebb6dddac21bdd207254
Joen <Joen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-26 13:47:05 +0000

    first commit
    git-svn-id: https://plugins.svn.wordpress.org/anti-feed-scraper-message/trunk@139395 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ anti-feed-scraper-message.php
@@ -0,0 +1,144 @@
+<?php
+/*
+Plugin Name: Anti Feed-Scraper Message
+Version: 0.9
+Description: Helps prevent your RSS feed from being "scraped" (copied and reposted elsewhere), by adding a customizable message with a link to your original post.
+Author: Joen
+Author URI: http://noscope.com/
+Plugin URI: http://noscope.com/
+*/
+
+
+//load_plugin_textdomain('afs', NULL, dirname(plugin_basename(__FILE__)));
+
+
+
+// Function
+function insertFeedMessage($output) {
+	
+	/**
+	* Show feed message if feed
+	*/
+	if ( is_feed() ) {
+	
+		$message = get_option('afs_message');
+		
+		// postname
+		$message = str_replace("[postname]", '<a href="'.get_permalink().'" rel="bookmark">'.get_the_title().'</a>', $message);
+		
+		// sitename
+		$message = str_replace("[sitename]", '<a href="'.get_bloginfo('home').'">'.get_bloginfo('name').'</a>', $message);
+		
+		// postdate
+		$message = str_replace("[postdate]", the_date(null, null, null, false) . get_the_time(), $message);
+		
+	}
+	
+
+	/**
+	* Output
+	*/
+	return $output . wpautop($message);
+
+
+}
+
+// Register functions
+add_filter('the_content', 'insertFeedMessage');
+
+
+
+
+
+
+/*****************************
+* Options Page
+*/
+// Options
+$afs_plugin_name = __("Anti Feed-Scraper", 'afs');
+$afs_plugin_filename = basename(__FILE__);
+
+add_option("afs_message", "[postname] originally appeared on [sitename] on [postdate].", "", "yes");
+
+
+
+
+function afs_admin_init() {
+	if ( function_exists('register_setting') ) {
+		register_setting('afs_settings', 'option-1', '');
+	}
+}
+function add_afs_option_page() {
+	global $wpdb;
+	global $afs_plugin_name;
+
+	add_options_page($afs_plugin_name . ' ' . __('Options', 'afs'), $afs_plugin_name, 8, basename(__FILE__), 'afs_options_page');
+	
+}
+add_action('admin_init', 'afs_admin_init');
+add_action('admin_menu', 'add_afs_option_page');
+
+
+
+// Options function
+function afs_options_page() {
+
+	if (isset($_POST['info_update'])) {
+			
+		// Update options
+		$afs_message = $_POST["afs_message"];
+		update_option("afs_message", $afs_message);
+
+
+		// Give an updated message
+		echo "<div class='updated fade'><p><strong>" . __('Options updated', 'afs') . "</strong></p></div>";
+		
+	}
+
+	// Show options page
+	?>
+
+		<div class="wrap">
+		
+			<div class="options">
+		
+				<form method="post" action="options-general.php?page=<?php global $afs_plugin_filename; echo $afs_plugin_filename; ?>">
+			
+				<h2><?php global $afs_plugin_name; printf(__('%s Settings', 'afs'), $afs_plugin_name); ?></h2>
+	
+		
+					<h3><?php _e("Feed Message", 'afs'); ?></h3>
+					<textarea cols="50" rows="10" name="afs_message" id="afs_message"><?php echo get_option('afs_message'); ?></textarea>
+
+					
+					<br />
+					
+					<p class="setting-description"><?php _e("Enter the message that will be shown after every post on your blog, but only in your feed. <strong>Remember</strong>, everybody can see this message, not only the 'scrape bots'."); ?>
+					</p>
+					<p class="setting-description"><?php _e("You can use these tags in your message: <code>[postname]</code>, <code>[sitename]</code>, <code>[postdate]</code>. They will be replaced with their counterparts. You can also use HTML, but remember to use inline CSS styles.", 'afs') ?></p>
+
+
+		
+					<p class="submit">
+						<?php if ( function_exists('settings_fields') ) settings_fields('afs_settings'); ?>
+						<input type='submit' name='info_update' value='<?php _e('Save Changes', 'afs'); ?>' />
+					</p>
+				
+				</form>
+				
+				
+			</div><?php //.options ?>
+			
+		</div>
+
+<?php
+}
+
+
+
+
+
+
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== Anti Feed-Scraper Message ===
+Contributors: Joen
+Tags: rss, xml, feed, atom, rss2, scrape, anti, prevention, spam
+Requires at least: 2.6
+Tested up to: 2.8.2
+Stable tag: 0.9
+
+== Description ==
+
+Helps prevent your RSS feed from being "scraped" (copied and reposted elsewhere), by adding a customizable message with a link to your original post.
+
+When the plugin is activated, a default message is shown. This message can then be configured in the options page. This is the default message:
+
+`[postname] originally appeared on [sitename] on [postdate].`
+
+[postname], [sitename] and [postdate] are automatically replaced by the plugin.
+
+== Installation ==
+
+1. Unpack the plugin, put it in your "plugins" folder (`/wp-content/plugins/`).
+2. Activate the plugin from the Plugins section.
+
+== Screenshots ==
+
+1. This shows the default message at the bottom of the post.
+2. This shows the options page where you can customize the message.
+
+== Changelog ==
+
+* 0.9: First release.
